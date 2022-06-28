f20603ab369c3dcb987de8fb9132737835fcd6c2
rubacodes <rubacodes@gmail.com>
2022-06-28 13:51:49 +0200

    rimosso category da product stock

--- classes/Product.php
+++ classes/Product.php
@@ -4,18 +4,17 @@ class Product
     private $product;
     private $price;
     private $stock;
-    private $category;
 
-    function __construct(string $_product, float $_price, int $_stock, string $_category)
+
+    function __construct(string $_product, float $_price, int $_stock)
     {
         $this->product = $_product;
         $this->price = $_price;
         $this->stock = $_stock;
-        $this->category = $_category;
     }
 }
 
-$product = new Product('osso di gomma', 12.50, 100, 'giochi');
+$product = new Product('osso di gomma', 12.50, 100);
 echo "<pre>";
 var_dump($product);
 echo "</pre>";
